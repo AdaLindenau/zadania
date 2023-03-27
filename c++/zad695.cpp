c895ba30dd863d9d3b3efccae685779636d06526
AdaLindenau <ada.lindenau@gmail.com>
2023-03-27 08:45:28 +0200

    zad

--- c++/zad695.cpp
+++ c++/zad695.cpp
@@ -2,34 +2,55 @@
 using namespace std;
 
 vector <int> wspolczynniki;
+int stopien;
 
-double funkcja(x1, x2)
+double F (double x)
 {
-
-
+double wynik = 0;
+int n = stopien;
+for (int w: wspolczynniki)
+{
+wynik = pow(x, n) * w;
+n--;
 }
+return wynik;
+}
+
+double funkcja(double x1, double x2)
+{
+int x3;
+while( abs(x1 - x2 < 0.001))
+{
+x3 = (x1 + x2)/2;
+if (F(x3) * F(x2) < 0) x1 = x3;
+else x2 = x3;
+}
+return x3;
+}
+
+
 
 int main()
 {
 ifstream plik ("metoda_polowienia.txt");
-int precision;
-int stopien;
+int ile;
+
 double x1;
 double x2;
 int temp;
-plik >> precision;
-for (int i = 0; i <11; i++)
+plik >> ile;
+for (int i = 0; i <ile; i++)
 {
 plik >> stopien;
-for (int j = 0; j <= 4; j++)
+for (int j = 0; j <= (stopien + 1); j++)
 {
 plik >> temp;
 wspolczynniki.push_back(temp);
 }
 plik >> x1;
 plik >> x2;
-cout.precision(precision);
 cout << funkcja(x1, x2) << endl;
+wspolczynniki.clear();
 }
 
 return 0;
