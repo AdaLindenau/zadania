2bcff97c83fb5e5dfa497e82816f32397e14b1e4
AdaLindenau <ada.lindenau@gmail.com>
2023-04-04 15:09:41 +0200

    dokończenie poprzedniego i początek 699

--- c++/zad 698.cpp
+++ c++/zad 698.cpp
@@ -1,11 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// SPRAWDZONY - DZIAŁA
+
 int na_odcinku = 0;
 int na_prostej = 0;
 double suma_odleglosci = 0;
 
-double odleglosc (pair<int,int> p1, pair<int,int> p2) // odległość działa
+double odleglosc (pair<int,int> p1, pair<double,double> p2) // odległość działa
 {
 double a = abs(p2.first - p1.first);
 double b = abs(p2.second - p1.second);
@@ -13,37 +15,38 @@ double c = sqrt((a*a) + (b*b));
 return c;
 }
 
-pair<double,double> punktPrzeciecia (double a, double b, pair<int,int> pz) // błędne wyniki
+pair<double,double> punktPrzeciecia (double a, double b, pair<int,int> pz)
 {
 double c = (-1 / a);
-double d = pz.second - (c* pz.first);
-double x = (d -b)/(a - c);
-double y = a * x + b;
-pair<int,int> punktPrzeciecia = make_pair(x, y);
+double d = pz.second -(c* pz.first);
+double x = (d - b)/(double)(a - c);
+double y = (a * x )+ b;
+
+pair<double,double> punktPrzeciecia = make_pair(x, y);
 return punktPrzeciecia;
 }
 
 void Zbadaj(pair<int,int> p1, pair<int,int> p2, pair<int,int> pz)
 {
-double a = (p2.second - p1.second) / (p2.first - p1.first);
+double a = (p2.second - p1.second) / (double)(p2.first - p1.first);
 double b = p1.second - (a * p1.first);
 
 if(pz.second - a * pz.first == b) //przynależnośćdo prostej
 {
 na_prostej++;
 
-if(odleglosc(pz, p1) < odleglosc(p1, p2) && odleglosc(pz, p2) < odleglosc(p1, p2)) na_odcinku++;
+if(odleglosc(pz, p1) <= odleglosc(p1, p2) && odleglosc(pz, p2) <= odleglosc(p1, p2)) na_odcinku++;
 }
 else
 {
-suma_odleglosci = odleglosc(pz, punktPrzeciecia(a, b, pz));
+suma_odleglosci += odleglosc(pz, punktPrzeciecia(a, b, pz));
 }
 
 }
 
 int main()
 {
-cout.precision(10);
+
 ifstream plik ("punkty_odcinki.txt");
 pair<int,int> p1, p2, pz;
 for(int i = 0; i < 100; i++)
@@ -52,8 +55,4 @@ plik >> p1.first >> p1.second >> p2.first >> p2.second >> pz.first  >> pz.second
 Zbadaj(p1, p2, pz);
 }
 cout << na_odcinku << endl << na_prostej << endl << suma_odleglosci << endl;
-
-pair<int,int>tempA = make_pair(0,0);
-pair<int,int>tempP = punktPrzeciecia(-1, 5, tempA);
-cout << tempP.first << " " << tempP.second;
 }
---
+++ c++/zad 699.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int na_obwodzie =0;
+int w_srodku = 0;
+
+double odleglosc (pair<int,int> p1, pair<double,double> p2)
+{
+double a = abs(p2.first - p1.first);
+double b = abs(p2.second - p1.second);
+double c = sqrt((a*a) + (b*b));
+return c;
+}
+
+pair<double,double> punktPrzeciecia (double a, double b, pair<int,int> pz)
+{
+double c = (-1 / a);
+double d = pz.second -(c* pz.first);
+double x = (d - b)/(double)(a - c);
+double y = (a * x )+ b;
+
+pair<double,double> punktPrzeciecia = make_pair(x, y);
+return punktPrzeciecia;
+}
+
+
+void Zbadaj(pair<double,double> p, vector<pair<double,double>> wierzcholki)
+{
+for(int i = 0; i < wierzcholki.size(); i++)
+{
+pair<double,double> p1,p2;
+p1 = wierzcholki[i];
+p2 = wierzcholki[i + 1];
+if (i == wierzcholki.size() -1) p2 = wierzcholki[0];
+
+double a = (p2.second - p1.second) / (double)(p2.first - p1.first);
+double b = p1.second - (a * p1.first);
+if(p.second - a * p.first == b && odleglosc(p, p1) <= odleglosc(p1, p2) && odleglosc(p, p2) <= odleglosc(p1, p2))
+{
+na_obwodzie++;
+return;
+}
+}
+}
+
+
+int main()
+{
+int boki;
+pair<double,double> p;
+vector<pair<double,double>> wierzcholki;
+ifstream plik ("wielokaty_punkty.txt");
+for(int i = 0; i < 200;i++)
+{
+plik >> boki;
+for(int i = 0; i << boki; i++)
+{
+double temp;
+double tempp;
+plik >> temp >> tempp;
+wierzcholki.push_back(make_pair(temp,tempp));
+}
+plik >> p.first;
+plik >> p.second;
+//koniec wczytania
+
+Zbadaj(p, wierzcholki);
+wierzcholki.empty();
+
+}
+cout << na_obwodzie  << " "<< w_srodku  << endl;
+
+
+return 0;
+}
