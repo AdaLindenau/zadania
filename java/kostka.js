d383b27a76e99e602a49fca07e4773fe5e0b9bb6
AdaLindenau <ada.lindenau@gmail.com>
2023-05-29 10:28:43 +0200

    kości

--- java/kostka.js
+++ java/kostka.js
@@ -1,24 +1,36 @@
 let klikniento = false;
 let interval;
 let ile = 0;
-
+let liczba;
+let czas = 30;
 function losuj()
 {
+    if(klikniento) return;
     klikniento = true;
     ile = 0;
-    interval = setInterval(obroc, 500);
-}    
-function wylosuj()
-{
-    return kostka + Math.ceil(Math.random()*6) + '.png';
-}
+    czas = 30;
+    liczba = Math.ceil(Math.random()*6)
+    setTimeout(obroc, czas);
+
+} 
 function obroc()
 {
     document.getElementById('kostka').src = wylosuj();
+    liczba ++;
+    if(liczba > 6) liczba = 1;
     ile++;
     if (ile > 5) 
     {
     clearInterval(interval);
+        ile = 0;
         klikniento = false;
     }
+    else
+    {
+        setTimeout(obroc,czas+=50);
+    }
+}   
+function wylosuj()
+{
+    return 'kostka/kostka' + liczba + '.png';
 }
